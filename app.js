59b3593de74e136c38fdcc26c099a4934ecd34df
Ilia <sorockiniliuh@yandex.ru>
2022-11-09 14:00:01 +0300

    pre sceleton

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
 require('@babel/register');
 const express = require('express');
+const ssr = require('./middleware/ssr');
 
 const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(ssr);
+app.use(express.static('public'));
+
+app.listen(3000);

8987743e37adfb94d3ed7ba0f87bd56664eb0482
Vasiliy <safecript@gmail.com>
2022-11-10 18:42:09 +0300

    Added seeds info

--- db/seeders/20221109094935-tariff.js
+++ db/seeders/20221109094935-tariff.js
@@ -1,25 +1,21 @@
-'use strict';
-
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+  async up(queryInterface) {
+    const tariff = [
+      { ageId: '1', dayId: '1', price: '1000' },
+      { ageId: '2', dayId: '1', price: '500' },
+      { ageId: '1', dayId: '2', price: '700' },
+      { ageId: '2', dayId: '2', price: '400' },
+    ];
+    const data = tariff.map((el) => ({
+      ...el,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    }));
+    await queryInterface.bulkInsert('Tariffs', data);
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
-  }
+  async down(queryInterface) {
+    await queryInterface.bulkDelete('Tariffs');
+  },
 };

49a18c34e2de22d5d6a72f3d337eba89d9c78184
Dima <adasd.s200@yandex.ru>
2022-11-10 18:34:49 +0300

    db fixed

--- db/models/age.js
+++ db/models/age.js
@@ -9,8 +9,9 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate({ Tariff }) {
+    static associate({ Tariff, Day }) {
       Age.Tariff = Age.hasMany(Tariff, { foreignKey: 'ageId' });
+      Age.Day = Age.belongsToMany(Day, { through: Tariff, foreignKey: 'ageId', otherKey: 'dayId' });
     }
   }
   Age.init({

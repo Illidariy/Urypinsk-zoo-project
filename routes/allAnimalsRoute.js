959555cb0ccbefb929e600620a94edb4d99489cb
Ilia <sorockiniliuh@yandex.ru>
2022-11-10 16:13:05 +0300

    galery is wirking

--- routes/allAnimalsRoute.js
+++ routes/allAnimalsRoute.js
@@ -2,10 +2,19 @@ const router = require('express').Router();
 
 const AllAnimalsPage = require('../view/AllAnimalsPage.jsx');
 const { Animal } = require('../db/models');
+const AnimalPage = require('../view/AnimalPage.jsx');
+const { Photo } = require('../db/models');
 
 router.get('/', async (req, res) => {
   const animals = await Animal.findAll({ row: true });
   res.renderComponent(AllAnimalsPage, { animals, title: 'Zoo' });
+});
+
+router.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  const galery = await Photo.findAll({ where: { animalId: id }, row: true });
+  // console.log(galery);
+  res.renderComponent(AnimalPage, { galery, title: 'Zoo' });
 });
 
 module.exports = router;
--- view/AnimalPage.jsx
+++ view/AnimalPage.jsx
@@ -1,2 +1,19 @@
 const React = require("react");
 const NavBar = require("./NavBar");
+const Layout = require("./Layout");
+
+function AnimalPage({ galery, title, user }) {
+  return (
+    <Layout title={title}>
+      <NavBar user={user} />
+      <div className="card-group">
+        {galery.map((photo) => (
+          <div key={photo.id}>
+            <img className="galery" src={photo.uri} alt="..." />
+          </div>
+        ))}
+      </div>
+    </Layout>
+  );
+}
+module.exports = AnimalPage;

84c721c32227dc6f5bc2679205f34c6cdcda01b8
Alexandra <lubnina.alexandra@gmail.com>
2022-11-11 08:56:33 +0300

    Fix bugs

--- routes/tariffRout.js
+++ routes/tariffRout.js
@@ -1,23 +1,21 @@
 const router = require('express').Router();
-const Tariffs = require('../view/Tariffs');
+const Tariffs = require('../view/Tariffs.jsx');
 const db = require('../db/models');
 
-
 router.get('/', async (req, res) => {
-  const myTar = await db.Tariff.findAll( {include: [{
-    association: db.Tariff.Day,
-    attributes: ['type'],
-  },
-{
-  association: db.Tariff.Age,
-    attributes: ['category'],
-}],
-attributes:['id', 'price'],
-raw: true,
-}
-)
+  const myTar = await db.Tariff.findAll({
+    include: [{
+      association: db.Tariff.Day,
+      attributes: ['type'],
+    },
+    {
+      association: db.Tariff.Age,
+      attributes: ['category'],
+    }],
+    attributes: ['id', 'price'],
+    raw: true,
+  });
   console.log(myTar);
-
 
   res.renderComponent(Tariffs);
 });
--- view/NavBar.jsx
+++ view/NavBar.jsx
@@ -2,8 +2,8 @@ const React = require('react')
 
 module.exports = function NavBar({ user }) {
   return (
-    <nav className="navbar navbar-dark bg-dark">     
-        <a className="navbar-brand" href="/">Zoo</a>
+    <nav className="navbar navbar-dark bg-dark" style={{ display: 'flex', justifyContent: 'space-around'}}>     
+        <a className="navbar-brand p-2" href="/">Zoo</a>
         <a className="navbar-brand" href="/animals">Animals</a>
         <a className="navbar-brand" href="/tariffs">Tariffs</a>
         {user && <a className="navbar-brand" href="/logout">Logout</a>}

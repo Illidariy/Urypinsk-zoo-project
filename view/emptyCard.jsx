0f9a8effc1f560b307a7c269c553b0e8c26e7b13
Alexandra <lubnina.alexandra@gmail.com>
2022-11-11 08:56:46 +0300

    Fix bugs

--- view/emptyCard.jsx
+++ view/emptyCard.jsx
@@ -6,10 +6,10 @@ module.exports = function emptyCard () {
       <div className="card-body">
         <form id='addForm' action='/animals' method='POST'>
         <h5 className="card-title animalName">name</h5>
-        <input name='name' type="text" onChange={(e) => console.log(e.target.value)} className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+        <input name='name' type="text"  className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
         <h5 className="card-text animalDescribe">describe</h5>
-        <input name="uri" type="file" onChange={(e) => console.log(e.target.value)}/>
-      <textarea name='describe' type="text" onChange={(e) => console.log(e.target.value)} className="form-control" id="exampleFormControlTextarea1" rows="17" />
+        <input name="uri" type="file" />
+      <textarea name='describe' type="text" className="form-control" id="exampleFormControlTextarea1" rows="17" />
       <button className="btn btn-primary addBtn" type='submit'>add</button>
         </form>
       </div>

9816ca421158425f3302f11d79a7e55ec6c9ab37
Dima <adasd.s200@yandex.ru>
2022-11-11 10:17:33 +0300

    delete working

--- public/js/deleteCard.js
+++ public/js/deleteCard.js
@@ -1,8 +1,9 @@
-const deleteBtn = document.querySelectorAll('.deleteCard');
+// const deleteBtn = document.querySelectorAll('.deleteCard');
+const cardGroup = document.querySelector('.card-group');
 
-deleteBtn.forEach((button) => {
-  button.addEventListener('click', async (event) => {
-    const cardAnimal = event.target.closest('.cardAnimal');
+cardGroup.addEventListener('click', async (e) => {
+  if (e.target.className.includes('deleteCard')) {
+    const cardAnimal = e.target.closest('.cardAnimal');
     const animalId = cardAnimal.dataset.id;
 
     const response = await fetch(`animals/${animalId}`, {
@@ -13,5 +14,21 @@ deleteBtn.forEach((button) => {
     } else {
       console.log('error');
     }
-  });
+  }
 });
+
+// deleteBtn.forEach((button) => {
+//   button.addEventListener('click', async (event) => {
+//     const cardAnimal = event.target.closest('.cardAnimal');
+//     const animalId = cardAnimal.dataset.id;
+
+//     const response = await fetch(`animals/${animalId}`, {
+//       method: 'delete',
+//     });
+//     if (response.ok) {
+//       cardAnimal.remove();
+//     } else {
+//       console.log('error');
+//     }
+//   });
+// });

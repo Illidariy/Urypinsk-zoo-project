afd30cdff7dc0eae69bc3594b310172b76571fe2
Ilia <sorockiniliuh@yandex.ru>
2022-11-11 10:53:34 +0300

    css

--- view/Main.jsx
+++ view/Main.jsx
@@ -1,14 +1,16 @@
-const React = require('react');
-const Layout = require('./Layout')
-const NavBar = require('./NavBar');
+const React = require("react");
+const Layout = require("./Layout");
+const NavBar = require("./NavBar");
 
-module.exports = function Main ({user}) {
+module.exports = function Main({ user }) {
   return (
     <Layout>
-      <NavBar user={user}/>
-      <div className='lemurImg'>
-        <h1>ДОБРО ПОЖАЛОВАТЬ В УРЮПИНСКИЙ ЗООПАРК</h1>
+      <NavBar user={user} />
+      <div className="lemurImg">
+        <h1 className="main-title">
+          ДОБРО ПОЖАЛОВАТЬ <br></br> В УРЮПИНСКИЙ ЗООПАРК
+        </h1>
       </div>
     </Layout>
-  )
-}
+  );
+};
